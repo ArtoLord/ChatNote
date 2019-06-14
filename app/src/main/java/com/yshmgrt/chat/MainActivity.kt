fdda2a2254cc1346aaae758afdc524b8afd68357
LDS2002 <fireman2002lds@gmail.com>
2019-06-14 10:27:51 +0300

    Add navigation view

--- app/src/main/java/com/yshmgrt/chat/MainActivity.kt
+++ app/src/main/java/com/yshmgrt/chat/MainActivity.kt
@@ -1,22 +1,61 @@
 package com.yshmgrt.chat
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import com.yshmgrt.chat.message.attachments.Image
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.drawer_layout.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerToggle: ActionBarDrawerToggle
+    lateinit var navigationController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.nav_app_bar_open_drawer_description, R.string.nav_app_bar_navigate_up_description)
+        drawer.addDrawerListener(drawerToggle)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
+
+        val host = fragment as NavHostFragment
+        navigationController = host.navController
+
+        to_notifies_button.setOnClickListener {
+            navigationController.navigate(R.id.notificationsFragment)
+            drawer.closeDrawers()
+        }
+        to_bookmarks_button.setOnClickListener {
+            navigationController.navigate(R.id.bookmarksFragment)
+            drawer.closeDrawers()
+        }
+        to_main_button.setOnClickListener {
+            navigationController.navigate(R.id.mainChatFragment)
+            drawer.closeDrawers()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_activity_toolbar, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true
+
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/yshmgrt/chat/view/MainChatFragment.kt
@@ -0,0 +1,14 @@
+package com.yshmgrt.chat.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yshmgrt.chat.R
+
+class MainChatFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.main_chat_fragment, container, false)
+    }
+}
