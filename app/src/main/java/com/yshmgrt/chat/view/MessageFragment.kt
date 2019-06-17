81c99983e9f19b6235c80dc6a84a3be038d4e0f1
ArtoLord <artgoi111@gmail.com>
2019-06-17 14:12:08 +0300

    some UI fixes

--- app/src/main/java/com/yshmgrt/chat/view/MessageFragment.kt
+++ app/src/main/java/com/yshmgrt/chat/view/MessageFragment.kt
@@ -24,13 +24,18 @@ class MessageFragment:Fragment() {
         val controller = Controller(context!!)
         controller.getMessageById(_id){
                 view.teg_field.removeAllViews()
-
-                val  markwon = Markwon.builder(context!!)
-                .usePlugin(CorePlugin.create())
-                .usePlugin(ImagesPlugin.create(context!!))
-                .build()
-            markwon.setMarkdown(view.message_text,it.text)
+            if(it.text.isEmpty()) view.message_text.visibility = View.GONE
+            else {
+                view.message_text.visibility = View.VISIBLE
+                val markwon = Markwon.builder(context!!)
+                    .usePlugin(CorePlugin.create())
+                    .usePlugin(ImagesPlugin.create(context!!))
+                    .build()
+                markwon.setMarkdown(view.message_text, it.text)
+            }
                 Log.d("WORK",it.toString())
+            if (it.tags.isEmpty()) view.teg_field.visibility = View.GONE
+            else view.teg_field.visibility = View.VISIBLE
                 for (i in it.tags) {
                     controller.getTagById(i) { exit ->
                         val tv = TagView(context!!)
