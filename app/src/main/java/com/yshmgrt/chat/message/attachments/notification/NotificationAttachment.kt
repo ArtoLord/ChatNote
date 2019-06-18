f76f4327415a0e5ffad060a90297e7aa78bf3592
ArtoLord <artgoi111@gmail.com>
2019-06-18 14:25:12 +0300

    implementing natification onSend

--- app/src/main/java/com/yshmgrt/chat/message/attachments/notification/NotificationAttachment.kt
+++ app/src/main/java/com/yshmgrt/chat/message/attachments/notification/NotificationAttachment.kt
@@ -2,12 +2,18 @@ package com.yshmgrt.chat.message.attachments.notification
 
 import android.content.Context
 import com.beust.klaxon.Klaxon
+import com.yshmgrt.chat.data_base.Controller
 import com.yshmgrt.chat.data_base.dataclasses.Attachment
+import com.yshmgrt.chat.data_base.dataclasses.SQL_Message
 import com.yshmgrt.chat.message.attachments.IAttachment
 
 class NotificationAttachment(val attachment: Attachment) : IAttachment {
     override fun onSended(context: Context) {
-
+        val controller = Controller(context)
+        controller.getMessageById(attachment.parentId){
+            val notification = Klaxon().parse<Notification>(attachment.link)
+            controller.resendMessage(attachment.parentId,notification!!.time)
+        }
     }
 
     private val notification = Klaxon().parse<Notification>(attachment.link)!!
