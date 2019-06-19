012156e0ddf8dc0dbfbf7f9d1f2dfcbe214d5f0b
LDS2002 <fireman2002lds@gmail.com>
2019-06-19 19:57:38 +0300

    Delete nav drawer

--- app/src/main/java/com/yshmgrt/chat/MainActivity.kt
+++ app/src/main/java/com/yshmgrt/chat/MainActivity.kt
@@ -5,43 +5,31 @@ import android.content.Intent
 import android.database.Cursor
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.Gravity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.yshmgrt.chat.view.BottomDrawerFragment
-import com.yshmgrt.chat.view.MainChatFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.drawer_layout.*
-import android.provider.MediaStore
-import android.util.Log
-import androidx.navigation.Navigation
 import org.jetbrains.anko.bundleOf
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var drawerToggle: ActionBarDrawerToggle
     lateinit var navigationController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-
-        drawerToggle = ActionBarDrawerToggle(this, drawer, R.string.nav_app_bar_open_drawer_description, R.string.nav_app_bar_navigate_up_description)
-        drawer.addDrawerListener(drawerToggle)
-        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        supportActionBar!!.setHomeButtonEnabled(true)
 
         val host = fragment as NavHostFragment
         navigationController = host.navController
@@ -52,19 +40,6 @@ class MainActivity : AppCompatActivity() {
             val bundle = bundleOf("messageId" to id)
 
             navigationController.navigate(R.id.action_mainChatFragment_to_messageFragment,bundle)
-        }
-
-        to_notifies_button.setOnClickListener {
-            navigationController.navigate(R.id.notificationsFragment)
-            drawer.closeDrawers()
-        }
-        to_bookmarks_button.setOnClickListener {
-            navigationController.navigate(R.id.bookmarksFragment)
-            drawer.closeDrawers()
-        }
-        to_main_button.setOnClickListener {
-            navigationController.navigate(R.id.mainChatFragment)
-            drawer.closeDrawers()
         }
     }
 
@@ -78,20 +53,6 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.main_activity_toolbar, menu)
         return super.onCreateOptionsMenu(menu)
     }
-
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        drawerToggle.syncState()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    var searchVisable = false
 
     fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) {
         this.addTextChangedListener(object : TextWatcher {
